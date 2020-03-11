cd90aa3811ad4e87a34c6875b7875b95f48f0da9
Nguyen Van Say <vansay@hotmail.fr>
2020-03-11 11:01:05 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,7 +18,7 @@ WIN_COMBINATIONS.each do |win_combination|
   position_1 = board[win_combination[0]]
   position_2 = board[win_combination[1]]
   position_3 = board[win_combination[2]]
-  binding.pry
+  
   if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 == "O" && position_2 == "O" && position_3 == "O"
     return win_combination
   else
