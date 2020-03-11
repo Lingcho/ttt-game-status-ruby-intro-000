92b4b0edbcf43d0bb1742f91ea7b1779b64743a9
Nguyen Van Say <vansay@hotmail.fr>
2020-03-11 11:04:16 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,13 +18,17 @@ WIN_COMBINATIONS.each do |win_combination|
   position_1 = board[win_combination[0]]
   position_2 = board[win_combination[1]]
   position_3 = board[win_combination[2]]
-  
+
   if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 == "O" && position_2 == "O" && position_3 == "O"
     return win_combination
   else
     false
   end
   end
+end
+
+def full?
+  
 end
 
 board = ["X", " ", "O", "X", " ", "O", " ", " ", "O"]
